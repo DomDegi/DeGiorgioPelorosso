acdbcbd6a0732e79365aa83c66aa61491c6157de
Domenico De Giorgio <10854350@polimi.it>
2026-06-06 20:21:50 +0200

    fix: Update image links to use raw GitHub URLs

--- src/__init__.py
+++ src/__init__.py
@@ -12,16 +12,14 @@ ingestion and monitoring pipeline for the **European Space Agency (ESA)**.
 The system is built on a highly modular and encapsulated architecture, designed 
 specifically to be robust and highly scalable in a multithreaded environment.
 
-![Component Diagram](../../uml/component_diagram.png)
-
+![Component Diagram](https://raw.githubusercontent.com/DomDegi/DeGiorgioPelorosso/main/uml/component_diagram.png)
 ---
 
 ### Object-Oriented Design (Class View)
 To ensure maintainability and testability, the Orchestrator is completely decoupled 
 from the concrete implementations via Abstract Base Classes (Interfaces). 
 
-![Class Diagram](../../uml/class_diagram.png)
-
+![Class Diagram](https://raw.githubusercontent.com/DomDegi/DeGiorgioPelorosso/main/uml/class_diagram.png)
 ---
 
 ### Navigation
